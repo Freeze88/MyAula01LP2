d970f7f5abf71d8e5f73a7398f33ed60ae425fc9
André Vitorino <andrevitorino1997@hotmail.com>
2019-10-02 16:54:48 +0100

    Add code

---
+++ MyAulda01/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyAulda01
+{
+    class Player
+    {
+        float Hp;
+        string type;
+
+        private void TakeDamage(float damage)
+        {
+            Hp = MathF.Max(0, Hp - damage);
+        }
+        private void Heal(float heal)
+        {
+            Hp = MathF.Min(100, Hp + heal);
+        }
+        private void Shoot(Player PL2)
+        {
+            PL2.TakeDamage(10);
+        }
+    }
+}
---
+++ MyAulda01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyAulda01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player pl1 = new Player();
+
+        }
+    }
+}
