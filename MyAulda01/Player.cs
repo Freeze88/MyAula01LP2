5c5408b6c956ef01b3e795d7aae38dc2e6de86c5
André Vitorino <andrevitorino1997@hotmail.com>
2019-10-02 18:08:23 +0100

    Modifications to use Getters and Setters

--- MyAulda01/Player.cs
+++ MyAulda01/Player.cs
@@ -6,18 +6,24 @@ namespace MyAulda01
 {
     class Player
     {
-        float Hp;
-        string type;
+        public float Hp { get => Hp; private set { Hp = (value < 0) ? 0 : (value > 100) ? 100 : value; } }
+        public string Skin { get; }
+        public static float MaxHp { get; } = 100f;
 
+        public Player(string skin)
+        {
+            this.Hp = MaxHp;
+            Skin = skin;
+        }
         private void TakeDamage(float damage)
         {
             Hp = MathF.Max(0, Hp - damage);
         }
         private void Heal(float heal)
         {
-            Hp = MathF.Min(100, Hp + heal);
+            Hp = MathF.Min(MaxHp, Hp + heal);
         }
-        private void Shoot(Player PL2)
+        public void Shoot(Player PL2)
         {
             PL2.TakeDamage(10);
         }
--- MyAulda01/Program.cs
+++ MyAulda01/Program.cs
@@ -6,8 +6,26 @@ namespace MyAulda01
     {
         static void Main(string[] args)
         {
-            Player pl1 = new Player();
+            Player pl1 = new Player( "bastard");
+            Player pl2 = new Player("Jhon_Wick");
 
+            bool loop = true;
+
+            while (loop)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "1")
+                    pl1.Shoot(pl2);
+                else if (input == "2")
+                    pl2.Shoot(pl1);
+                else if (input == "3")
+                    loop = !loop;
+
+                Console.WriteLine("Pl1 Hp =" + pl1.Hp);
+                Console.WriteLine("Pl2 Hp =" + pl2.Hp);
+
+            }
         }
     }
 }
